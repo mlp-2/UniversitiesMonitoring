640b6c00f9de6315fd65b4081c5f19fee5ca5560
Denis Voitenko <denis@denvot.dev>
2023-01-08 23:10:32 +0300

    Добавление метода расширения для добавления контекста

---
+++ UniversitiesMonitoring.Data/ServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniversityMonitoring.Data.Models;
+using UniversityMonitoring.Data.Repositories;
+
+namespace UniversityMonitoring.Data;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddDataContext(this IServiceCollection collection, IConfiguration configuration) =>
+        collection.AddDbContext<UniversitiesMonitoringContext>(options => 
+            options.UseMySql(
+                configuration.GetConnectionString("UniversitiesMonitoring"),
+                new MySqlServerVersion(new Version(8, 0, 29)))
+            .UseLazyLoadingProxies())
+            .AddScoped<IDataProvider, DataProvider>();
+}

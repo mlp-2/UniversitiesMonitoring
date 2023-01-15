02d5b814e383b2e02653b0ddcbe2dd4692106790
Denis Voitenko <denis@denvot.dev>
2023-01-15 18:22:59 +0300

    Реализация IWebSocketUpdateStateNotifier

---
+++ UniversitiesMonitoring.Api/WebSocket/WebSocketUpdateStateNotifier.cs
@@ -0,0 +1,72 @@
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+using UniversitiesMonitoring.Api.Entities;
+using UniversityMonitoring.Data.Models;
+using UniversityMonitoring.Data.Repositories;
+using WS = System.Net.WebSockets.WebSocket;
+
+namespace UniversitiesMonitoring.Api.WebSocket;
+
+public class WebSocketUpdateStateNotifier : IWebSocketUpdateStateNotifier
+{
+    private readonly List<Tuple<WS, TaskCompletionSource<object>>> _webSockets = new();
+    private readonly IRepository<UniversityService, ulong> _universitiesServices;
+
+    public WebSocketUpdateStateNotifier(IDataProvider provider)
+    {
+        _universitiesServices = provider.UniversityServices;
+    }
+    
+    public void AppendWebSocket(WS webSocket, TaskCompletionSource<object> socketFinishedTcs)
+    {
+        _webSockets.Add(new Tuple<WS, TaskCompletionSource<object>>(webSocket, socketFinishedTcs));
+    }
+
+    public async Task NotifyAsync(ulong[] servicesIds)
+    {
+        await using var changesDataStream = new MemoryStream();
+        var badSockets = new List<Tuple<WS, TaskCompletionSource<object>>>();
+        
+        var changes = await CreateChangeStatesReportsAsync(servicesIds);
+        var changesJsonBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(changes));
+
+        foreach (var webSocketTup in _webSockets)
+        {
+            var webSocket = webSocketTup.Item1;
+            var tcs = webSocketTup.Item2;
+            
+            if (webSocket.CloseStatus.HasValue)
+            {
+                badSockets.Add(webSocketTup);
+                tcs.SetResult(new object());
+            }
+
+            await webSocket.SendAsync(changesJsonBytes, WebSocketMessageType.Text, 
+                                        WebSocketMessageFlags.EndOfMessage, CancellationToken.None);
+        }
+
+        _webSockets.RemoveAll(x => badSockets.Contains(x));
+    }
+
+    private async Task<UniversityServiceChangeStateEntity[]> CreateChangeStatesReportsAsync(ulong[] servicesIds)
+    {
+        var changes = new UniversityServiceChangeStateEntity[servicesIds.Length];
+
+        for (var serviceIndex = 0; serviceIndex < servicesIds.Length; serviceIndex++)
+        {
+            var serviceId = servicesIds[serviceIndex];
+            var service = await _universitiesServices.FindAsync(serviceId);
+
+            if (service == null)
+            {
+                throw new InvalidOperationException($"Service with {service} hasn't found");
+            }
+
+            var serviceStatus = service.UniversityServiceStateChanges.OrderByDescending(x => x.ChangedAt).FirstOrDefault()?.IsOnline ?? false;
+            changes[serviceIndex] = new UniversityServiceChangeStateEntity(serviceId, service.UniversityId, service.Name, service.University.Name, serviceStatus);
+        }
+
+        return changes;
+    }
+}

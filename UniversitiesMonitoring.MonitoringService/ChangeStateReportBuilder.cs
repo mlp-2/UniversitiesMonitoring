99b7b6f1225d13801955063616fbed23b315bcc4
Denis Voitenko <denis@denvot.dev>
2023-01-20 11:06:23 +0300

    Создание шаблона

---
+++ UniversitiesMonitoring.MonitoringService/ChangeStateReportBuilder.cs
@@ -0,0 +1,16 @@
+using UniversityMonitoring.Data.Entities;
+
+namespace UniversitiesMonitoring.MonitoringService;
+
+internal class ChangeStateReportBuilder
+{
+    public void AddReport(ulong serviceId, bool isOnline)
+    {
+        throw new NotImplementedException();
+    }
+
+    public UpdateEntity BuildReport()
+    {
+        throw new NotImplementedException();
+    }
+}

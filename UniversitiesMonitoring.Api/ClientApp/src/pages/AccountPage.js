ef61fef2b5506a9d19155779abeb4ff3bd599848
Denis Voitenko <denis@denvot.dev>
2023-02-13 15:32:26 +0300

    Неоптимизирвоанная верстка

--- UniversitiesMonitoring.Api/ClientApp/src/pages/AccountPage.js
+++ UniversitiesMonitoring.Api/ClientApp/src/pages/AccountPage.js
@@ -1,15 +1,105 @@
 import {useEffect, useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {TextInput} from "../components/TextInput";
+import {SubmitButton} from "../components/SubmitButton";
+import {createUseStyles} from "react-jss";
+import FireBall from "../assets/images/fireball.svg"
+
+const useStyles = createUseStyles({
+   editEmailForm: {
+       display: "flex",
+       flexDirection: "column",
+       width: "30vw",
+       "& input::placeholder": {
+            fontSize: 18  
+       },
+       "& div": {
+           display: "flex",
+           flexDirection: "column",
+           gap: 10
+       },
+       "& .checkbox-combo": {
+           flexDirection: "row",
+           fontSize: 15
+       },
+       "& input[type=submit]": {
+           width: "30%",
+           fontSize: 15
+       }
+   },
+    layout: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        height: "100vh",
+        background: "#0798EA"
+    },
+    container: {
+       background: "#FFF",
+        padding: 20,
+        borderRadius: 15
+    },
+    formsWrapper: {
+        display: "flex",
+        flexDirection: "column",
+        gap: 30,
+        fontSize: 20,
+        "& .fa-user": {
+            marginRight: 10,
+            color: "#0798EA"
+        },
+        "& .username": {
+            fontWeight: "bold"
+        },
+        "& .fa-user, .username": {
+            fontSize: 24
+        }
+    }
+});
 
 export function AccountPage() {
-    const [user, setUser] = useState();
+    const [user, setUser] = useState(null);
+    const style = useStyles();
     
     useEffect(() => {
         setUser(getUser());
     }, []);
     
     if (user === null) return <span>Загрузка</span>
+    
+    return <div className={style.layout}>
+        <div className={style.formsWrapper}>
+            <div className={style.container}>
+                <FontAwesomeIcon icon={faUser}/>
+                <span className="username">@{user.username}</span>
+            </div>
+            <div className={style.container}>
+                <form method="put" className={style.editEmailForm}>
+                    <div>
+                        <label htmlFor="email">Email для рассылки:</label>
+                        <TextInput placeholder="name@domain.ru" name="email" id="email"/>    
+                    </div>
+                    <div className="checkbox-combo">
+                        <label htmlFor="canSend">Присылайте мне уведомления</label>
+                        <input type="checkbox" name="canSend" id="canSend" defaultChecked={user.sendToEmail}/>    
+                    </div>
+                    <SubmitButton content="Изменить" disabled={true}/>
+                </form>
+            </div>
+        </div>
+        <div>
+            <img src={FireBall} alt="Что-то пошло не так"/>
+        </div>
+    </div>
 }
 
 function getUser() {
-    
+    return {
+        id: 0,
+        username: "DenVot",
+        email: "denis@denvot.dev",
+        sendToEmail: true
+    }
 }

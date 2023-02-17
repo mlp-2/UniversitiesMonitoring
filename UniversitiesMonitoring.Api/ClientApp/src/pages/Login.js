81fa50b50d75e8d8e0985f684cbf8fe06d0b80cd
Denis Voitenko <denis@denvot.dev>
2023-02-17 14:33:23 +0300

    Адаптация страницы логина

--- UniversitiesMonitoring.Api/ClientApp/src/pages/Login.js
+++ UniversitiesMonitoring.Api/ClientApp/src/pages/Login.js
@@ -1,16 +1,13 @@
 import {WelcomePage} from "./WelcomePage";
-import {TextInput} from "../components/TextInput";
 import {createUseStyles} from "react-jss";
 import {Link, Navigate} from "react-router-dom";
 import Constants from "../Constants";
-import {SubmitButton} from "../components/SubmitButton";
 import axios from "axios";
 import {useEffect, useState} from "react";
-import {Button} from "../components/Button";
 import useGlobalStyles from "../GlobalStyle";
-import {Container} from "reactstrap";
 import Swal from "sweetalert2";
 import {GetUser} from "../ApiMethods";
+import {Button, Container, Form} from "react-bootstrap";
 
 const useStyles = createUseStyles({
     formStyle: {
@@ -28,8 +25,7 @@ const useStyles = createUseStyles({
             width: "100%"
         },
         "& button": {
-            width: "50%",
-            marginTop: "6vh"
+            width: "50%"
         }
     },
     frameStyle: {
@@ -40,7 +36,7 @@ const useStyles = createUseStyles({
         height: "80%"
     },
     registrationLabel: {
-        fontSize: 24,
+        fontSize: 16,
         "& a": {
             color: Constants.brandColor,
             textDecoration: "none"
@@ -52,7 +48,7 @@ const useStyles = createUseStyles({
         justifyContent: "center",
         gap: 20,
         "& span": {
-            fontSize: "32px",
+            fontSize: "16px",
             textAlign: "center"
         },
         "& button": {
@@ -63,75 +59,31 @@ const useStyles = createUseStyles({
 
 export function Login() {
     const style = useStyles();
-    const [authAccepted, setDefAuthAccepted] = useState(null);
     const [user, setUser] = useState(null);
-    const globalStyle = useGlobalStyles();
     
     useEffect(() => {
         async function getLogin() {
-            const defaultAuth = await GetUser();
+            try {
+                const defaultAuth = await GetUser();
 
-            if (defaultAuth !== null) {
-                setUser(defaultAuth);
+                if (defaultAuth !== null) {
+                    setUser(defaultAuth);
+                }
+
+                console.info("Default user found")
+            } catch {
+                console.info("Default user not found. Using form auth strategy")
             }
         }
         getLogin();
     }, []);
     
-    
-    if (authAccepted !== null) {
-        if (authAccepted) {
-            return <Navigate to="/universities-list"/>;
-        } 
-        
-        localStorage.setItem("token", undefined);
-        setUser(null);
-        setDefAuthAccepted(null);
-    }
-    
-    async function handleSubmit(e) {
-        e.preventDefault();
-        
-        const form = e.target;
-        const formData = new FormData(form);
-        
-        try {
-            const response = await axios.post("/api/user/auth", Object.fromEntries(formData.entries()));
-
-            localStorage.setItem("token", response.data.jwt)
-            setUser(await GetUser());
-
-            setDefAuthAccepted(true);
-        } catch (axiosError) {
-            const response = axiosError.response;
-            
-            await Swal.fire({
-                icon: "error",
-                title: response.status !== 500 ? "Неверный логин или пароль" :
-                    "Что-то пошло не так...",
-                showConfirmButton: false,
-                timer: 2000
-            });
-        }
-    }
-    
     return <WelcomePage>
         <div className={style.frameStyle}>
             {
-                user !== null ?  <div className={style.defaultAuthContainer}>
-                        <Container className="d-flex flex-column align-items-center justify-content-center gap-2">
-                            <span>Это Вы, <b><span className={globalStyle.brandFontColored}>@{user.username}</span></b>?</span>
-                            <Button onClick={() => setDefAuthAccepted(true)}>Да</Button>
-                            <Button onClick={() => setDefAuthAccepted(false)} style={{background: "#FF4D15"}}>Нет, выйти</Button>
-                        </Container>
-                    </div> :
-                    <form method="post" className={style.formStyle} onSubmit={handleSubmit}>
-                        <div>
-                            <TextInput type="text" name="username" placeholder="Логин"/>
-                            <TextInput type="password" name="password" placeholder="Пароль"/>
-                        </div>
-                        <SubmitButton value="Войти"/>
-                    </form>
+                user !== null ?  <DefaultAuth user={user} 
+                                              throwNotThisUser={() => setUser(null)}/> :
+                    <FormAuth/>
             }
             <div>
                 <span className={style.registrationLabel}>
@@ -140,4 +92,71 @@ export function Login() {
             </div>
         </div>
     </WelcomePage>
+}
+
+function DefaultAuth({user, throwNotThisUser}) {
+    const [accepted, setAccepted] = useState(false);
+    
+    const style = useStyles();
+    const globalStyle = useGlobalStyles();
+    
+    function handleResult(res) {
+        if (res) {
+            setAccepted(true);
+        } else {
+            throwNotThisUser();
+        }
+    }
+    
+    if (accepted) return <Navigate to="/universities-list"/>;
+    
+    return  <div className={style.defaultAuthContainer}>
+        <Container className="d-flex flex-column align-items-center justify-content-center gap-2">
+            <span>Это Вы, <b><span className={globalStyle.brandFontColored}>@{user.username}</span></b>?</span>
+            <Button onClick={() => handleResult(true)}>Да</Button>
+            <Button variant="danger" onClick={() => handleResult(false)}>Нет, выйти</Button>
+        </Container>
+    </div>;
+}
+
+function FormAuth() {
+    const [formDialogFinished, setFinished] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const style = useStyles();
+    
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setLoading(true);
+        const form = e.target;
+        const formData = new FormData(form);
+
+        try {
+            const response = await axios.post("/api/user/auth", Object.fromEntries(formData.entries()));
+
+            localStorage.setItem("token", response.data.jwt)
+
+            setFinished(true);
+        } catch (axiosError) {
+            const response = axiosError.response;
+
+            await Swal.fire({
+                icon: "error",
+                title: response.status !== 500 ? "Неверный логин или пароль" :
+                    "Что-то пошло не так...",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        }
+        
+        setLoading(false);
+    }
+    
+    if (formDialogFinished) return <Navigate to="/universities-list"/>;
+    
+    return <Form method="post" className={style.formStyle} onSubmit={handleSubmit}>
+        <Form.Control disabled={loading} className="mb-3" type="text" placeholder="Логин" name="username"/>
+        <Form.Control disabled={loading} className="mb-3" type="password" placeholder="Пароль" name="password"/>
+        <Button disabled={loading} type="submit">Войти</Button>
+    </Form>;
 }

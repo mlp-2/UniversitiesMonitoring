6b8aacb216db070429a1a3a2017f7401d299fd98
Denis Voitenko <denis@denvot.dev>
2023-03-16 11:56:15 +0300

    Добавление кэшерования времени ответа

---
+++ UniversitiesMonitoring.Api/ResponseStatistics.cs
@@ -0,0 +1,20 @@
+namespace UniversitiesMonitoring.Api;
+
+/// <summary>
+/// Сводка статистики ответов на HEAD заросы с сервиса
+/// </summary>
+public class ResponseStatistics
+{
+   private const int QueueCapacity = 20;
+
+   private readonly List<long> _responsesTime = new(QueueCapacity);
+
+   public void AddResponseData(long responseTime) => _responsesTime.Add(responseTime);
+   
+   /// <summary>
+   /// Проверяет по времени ответа потенциальную атаку
+   /// </summary>
+   /// <param name="responseTime">Время ответа</param>
+   /// <returns>true, если сервис атакован</returns>
+   public bool IsPotentialAttack(long responseTime) => responseTime >= _responsesTime.Average() * 2;
+}

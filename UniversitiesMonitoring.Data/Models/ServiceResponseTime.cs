05960e6b2696159132eaebc801ff1110c0e795c0
Denis Voitenko <denis@denvot.dev>
2023-03-16 12:35:58 +0300

    Добавление времени ответа сервиса в модель данных

---
+++ UniversitiesMonitoring.Data/Models/ServiceResponseTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniversityMonitoring.Data.Models
+{
+    public partial class ServiceResponseTime
+    {
+        public ulong Id { get; set; }
+        public long ResponseTime { get; set; }
+        public ulong ServiceId { get; set; }
+
+        public virtual UniversityService Service { get; set; } = null!;
+    }
+}
